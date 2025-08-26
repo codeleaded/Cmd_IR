c017d24808963210cbb67c2b1b21c193908d6b29
codeleaded <codeleaded@gmail.com>
2025-08-26 20:38:49 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,14 @@
+#include "/home/codeleaded/System/Static/Library/IR.h"
 
 int main(int argc,char** argv){
+    if(argc < 2){
+        printf("[IRRuntime]: Arguments -> Usage: irr <.iralx>\n");
+        return 0;
+    }
+
+    IRRuntime irr = IRRuntime_New(argv[1],"./bin");
+    IRRuntime_Print(&irr);
+    IRRuntime_Free(&irr);
+
     return 0;
 }
