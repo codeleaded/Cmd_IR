5b25f14d1586b15fbe32e7ada91a203ac5f45ff2
codeleaded <codeleaded@gmail.com>
2025-12-01 04:15:51 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,26 +1,21 @@
 #include "/home/codeleaded/System/Static/Library/IRRuntime.h"
 
 int main(int argc,char** argv){
-    if(argc < 2){
-        printf("[IRRuntime]: Arguments -> Usage: irr <.iralx>\n");
-        return 0;
-    }
+    //if(argc < 2){
+    //    printf("[IRRuntime]: Arguments -> Usage: irr <.iralx>\n");
+    //    return 0;
+    //}
 
-    IRRuntime irr = IRRuntime_New("./code/Main.iralx","./bin");
-    //IRRuntime_Print(&irr);
+    IRRuntime irr = IRRuntime_New("./code/IntraIR.iralx","./bin");
+    IRRuntime_Print(&irr);
 
     Variable params[] = {
         Variable_Make(".RETURN","int",(Number[]){ 0LL },sizeof(Number),-1,NULL,NULL),
         VARIABLE_END
     };
     IRRuntime_Function(&irr,"main",params);
-
-    IRRuntime_PrintVariable(&irr,params + 0);
-    for(int i = 0;i<sizeof(params) / sizeof(Variable) - 1;i++){
-        Variable_Free(params + i);
-    }
+    //IRRuntime_PrintVariable(&irr,params + 0);
 
     IRRuntime_Free(&irr);
-
     return 0;
 }
