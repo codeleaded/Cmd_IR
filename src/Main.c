a5686ea131f8a14196e1c18c075e801bf25f5ad4
codeleaded <codeleaded@gmail.com>
2026-01-03 04:38:04 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,4 @@
-#include "/home/codeleaded/System/Static/Library/ASMRuntime.h"
+#include "/home/codeleaded/System/Static/Library/IRVM.h"
 
 int main(int argc,char** argv){
     //if(argc < 2){
@@ -6,16 +6,8 @@ int main(int argc,char** argv){
     //    return 0;
     //}
 
-    ASMRuntime irr = ASMRuntime_New("./code/Main.asmalx","./bin");
-    ASMRuntime_Print(&irr);
-
-    Variable params[] = {
-        Variable_Make("a","int",(Number[]){ 0LL },sizeof(Number),-1,NULL,NULL),
-        VARIABLE_END
-    };
-    ASMRuntime_Function(&irr,"main",params);
-    //ASMRuntime_PrintVariable(&irr,params + 0);
-
-    ASMRuntime_Free(&irr);
+    IRVM irr = IRVM_Make("./code/Main."  IRVM_TYPE,"./bin");
+    IRVM_Print(&irr);
+    IRVM_Free(&irr);
     return 0;
 }
